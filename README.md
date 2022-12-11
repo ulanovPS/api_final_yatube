5baf197a1028a343b5ead620098beb3b5f215d6b
Павел Уланов <p.s.ulanov@gmail.com>
2022-12-11 20:00:36 +0500

    first

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# api_final
-api final
+# Description of the project
+
+API for the yatube project.
+
+Done:
+
+Viewsets used.
+
+Authentication uses JWT tokens.
+
+Unauthenticated users have read-only access to the API. The exception is the /follow/ endpoint.
+
+Authenticated users are allowed to change and delete their content; otherwise, access is read-only.
+
+# Installation
+
+## 1) Clone Git Clone repository
+## 2) Go to a new project folder and create and activate a virtual environment for the project
+
+python -m venv venv
+
+source venv/scripts/activate
+
+## 3) Install dependencies
+python pip install -r requirements.txt
+
+## 4) Make migrations
+python manage.py makemigrations
+python manage.py migrate
+
+## 5) Start the server
+python manage.py runserver
+
+# Examples
+
+To access the API, you need to get a token:
+You need to make a POST request to localhost:8000/api/v1/token/ passing the username and password fields.
+The API will return a JWT token
+
+Further, by passing the token, it will be possible to access methods, for example:
+
+/api/v1/posts/ (GET, POST, PUT, PATCH, DELETE)
+
+When sending a request, pass the token in the header Authorization: Bearer <token>
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,23 +4,92 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
-    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='posts')
+        User,
+        on_delete=models.CASCADE
+    )
     image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+        upload_to='posts/',
+        null=True,
+        blank=True
+    )
+    group = models.ForeignKey(Group,
+                              on_delete=models.SET_NULL,
+                              blank=True,
+                              null=True,)
 
     def __str__(self):
         return self.text
 
+    class Meta:
+        default_related_name = 'posts'
+
 
 class Comment(models.Model):
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='comments')
+        User,
+        on_delete=models.CASCADE,
+    )
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name='comments')
+        Post,
+        on_delete=models.CASCADE
+    )
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
+
+    class Meta:
+        default_related_name = 'comments'
+
+    def __str__(self):
+        return '"{}" to post "{}" by author "{}"'.format(
+            self.text,
+            self.post,
+            self.author
+        )
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='following'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_user_subscribers'
+            )
+        ]
+
+    def __str__(self):
+        return '{} follows {}'.format(
+            self.user,
+            self.following
+        )
